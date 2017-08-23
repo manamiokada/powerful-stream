a3e2d096febde55b91ca32d40483d98394c43d96
Manami Okada <manamiokada@Manamis-MacBook-Pro.local>
2017-08-23 16:17:14 -0700

    readd procfile and fix port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,4 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
