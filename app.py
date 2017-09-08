ca87a9ba10cf2c6a9cd0d1a80509653a0344068d
Manami Okada <manamiokada@Manamis-MacBook-Pro.local>
2017-09-08 05:26:04 -0700

    Added server stop api endpoint

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html', user='janedoe', title='HOME')
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/stop', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
