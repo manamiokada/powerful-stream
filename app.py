f38600bc7c65609cbbeaa1a62a823f3e4d397f33
Manami Okada <manamiokada@Manamis-MacBook-Pro.local>
2017-09-10 01:29:19 -0700

    Initialize flickr api

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import os
+import conf
+import flickrapi
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
+def init():
+    global flickr
+    flickr = flickrapi.FlickrAPI(conf.key, conf.secret, format='parsed-json')
+
 @app.route('/')
 def index():
-    return render_template('index.html', user='janedoe', title='HOME')
+    photoset = flickr.photosets.getPhotos(photoset_id='72157688275976596', user_id='97801184@N07')
+    return render_template('index.html', user='janedoe', title='HOME', photoset=photoset['photoset']['photo'])
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -21,4 +29,6 @@ def shutdown():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    init()
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
