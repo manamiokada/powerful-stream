55f24cbffea320a365c03c438b545098afbc1ea9
Manami Okada <manamiokada@Manamis-MacBook-Pro.local>
2017-09-10 01:34:03 -0700

    remove server stop endpoint

--- app.py
+++ app.py
@@ -14,21 +14,8 @@ def index():
     photoset = flickr.photosets.getPhotos(photoset_id='72157688275976596', user_id='97801184@N07')
     return render_template('index.html', user='janedoe', title='HOME', photoset=photoset['photoset']['photo'])
 
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-
-@app.route('/stop', methods=['GET'])
-def shutdown():
-    shutdown_server()
-    return 'Server shutting down...'
-
-
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     init()
-    app.debug = True
     app.run(host='0.0.0.0', port=port)
